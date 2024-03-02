4218840d2c38600be67fa4679e0b5ce84ebbea0c
ViditChawda <viditchawda301@gmail.com>
2024-03-02 13:16:12 +0530

    login route

--- server/index.js
+++ server/index.js
@@ -4,30 +4,58 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user_model')
 
-mongoose.connect('mongodb+srv://viditchawda301:T0hhjTL2mbdh6icM@cluster0.xnqc0lr.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://viditchawda301:T0hhjTL2mbdh6icM@cluster0.xnqc0lr.mongodb.net/Authentication?retryWrites=true&w=majority')
 
 app.use(cors())
 app.use(express.json())
 
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+    console.log('MongoDB connected successfully');
+});
+
 app.post('/api/register', async (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     try {
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
-        })
-        res.send({ status: 'ok' })
+        });
+        res.status(201).json({ status: 'ok', user });
     } catch (err) {
-        res.json({ status: 'error', error: 'Duplicate Email' })
+        console.log(err)
+        if (err.code === 11000 && err.keyPattern && err.keyPattern.email) {
+            res.status(400).json({ status: 'error', error: 'Duplicate Email' });
+        } else {
+            // Other errors
+            res.status(500).json({ status: 'error', error: 'Internal Server Error' });
+        }
     }
-})
+});
 
-app.post('api/login', async (req, res) => {
-    const user = await User.findOne({
-        email: req.body.email,
-        password: req.body.password
-    })
+
+app.post('/api/login', async (req, res) => {
+    console.log('ajsdf', req.body)
+    try {
+        const user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password
+        })
+        console.log(user)
+        if (user) {
+            return res.json({ status: 'ok', user: true })
+        } else {
+            return res.json({ status: 'error', user: false })
+        }
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
 })
 
 app.listen(1337, () => {
--- server/models/user_model.js
+++ server/models/user_model.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose')
 
 const User = new mongoose.Schema(
     {
-        name : {type : String, required : true},
-        email : {type : String, required : true, unique : true},
-        password : {type : String, required : true},
-        quote : {type : String}
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        quote: { type: String }
     },
-    {collection : 'user-data'}
+    { collection: 'user-data' }
 )
 
 const model = mongoose.model('UserData', User)
 
-module.exports = module
+module.exports = model
